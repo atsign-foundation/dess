85745d5d5695917142be97bcb6c8776a8216d01d
cconstab <colin@constable.cc>
2021-05-14 23:49:30 -0700

    Update setup.sh

--- dess/setup.sh
+++ dess/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Source the environment
 source .env
+if [[ "${ATSIGN}" == "@sign" ]]
+        then
+        tput setaf 1
+        echo Please edit the .env file before running setup.sh
+        tput setaf 9
+        exit 1
+fi
 # Put a directory in place for the secondary
 mkdir ~/atsign/$ATSIGN
 # Pu the root SSL certs in place
