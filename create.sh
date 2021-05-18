c1f0cf682370b135c8dc10a956522ae5934ddb56
cconstab <colin@constable.cc>
2021-05-17 19:22:34 -0700

    Update create.sh

--- create.sh
+++ create.sh
@@ -70,7 +70,7 @@ fi
 # Copy files in from base
 sudo -u atsign mkdir -p ~atsign/dess/$ATSIGN
 sudo -u atsign cp  base/.env ~atsign/dess/$ATSIGN
-sudo -u atsign cp  base/docker-compose ~atsign/dess/$ATSIGN
+sudo -u atsign cp  base/docker-compose.yaml ~atsign/dess/$ATSIGN
 # Make the edits to the .env file
 # First comment out everything
 sudo -u atsign sed -i 's/^\([^#].*\)/# \1/g' ~atsign/dess/$ATSIGN/.env
