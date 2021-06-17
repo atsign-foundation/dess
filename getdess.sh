3832fb6ebac4dfe171f8a97a66255c52f2d27e7c
XavierChanth <xchanthavong@gmail.com>
2021-06-17 16:15:40 -0400

    outline curl install script

--- getdess.sh
+++ getdess.sh
@@ -12,3 +12,55 @@ set -m
 
 # Container Dependencies
 # fuse squashfuse
+
+
+preinstall () {
+  # get user's release
+  # get package manager
+  # update package repos
+}
+
+install_dependencies () {
+  # openssl, qrencode, curl
+  # if container
+  # install fuse squashfuse
+}
+
+install_certbot () {
+  # install snapd
+  # install snap core
+  # enable and link snap
+  # install certbot
+}
+
+install_docker () {
+  # docker, docker compose
+}
+
+setup_atsign_user () {
+  # add atsign user
+  # link lets-encrypt folder
+  # make ~atsign directories
+  # curl the base files for atsign
+}
+
+setup_docker () {
+  # curl the base/shepherd.yaml file to atsign
+  # give atsign user docker permissions
+  # setup and deploy the swarm as atsign
+}
+
+test_atsign_user () {
+  # check if docker works for atsign user
+}
+
+get_dess_scripts () {
+  # curl create and reshowqr from repo
+  # to /usr/local/bin
+}
+
+do_install () {
+
+}
+
+do_install
