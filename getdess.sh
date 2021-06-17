b9ef2a1cf8c9bb6e3257cb6d974482f192f49ea4
XavierChanth <xchanthavong@gmail.com>
2021-06-17 15:32:19 -0400

    add getdess.sh

---
+++ getdess.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Enable job control
+set -m
+
+# Dess installation
+
+# Dependencies
+# openssl, qrencode
+# docker, docker-compose, snapd
+# certbot (via snap)
+
+# Container Dependencies
+# fuse squashfuse
