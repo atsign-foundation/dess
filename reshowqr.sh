c5e0518701b07fb6a5ae4459ce134d6fc3008eb5
Chris Swan <478926+cpswan@users.noreply.github.com>
2021-06-17 08:47:33 +0000

    Improved validation

--- reshowqr.sh
+++ reshowqr.sh
@@ -4,5 +4,10 @@ if [[ $# -eq 0 || $# -gt 1 ]] ; then
     echo "Example ./reshowqr.sh @bob"
     exit 1 
 fi
+if [[ ! -f ~atsign/dess/$1/.env ]]
+then
+    echo "Can't find $1, please check for typos."
+    exit 2
+fi
 . ~atsign/dess/$1/.env
 qrencode -t ANSIUTF8 "${ATSIGN}:${SECRET}"
