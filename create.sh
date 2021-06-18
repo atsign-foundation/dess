115a832db816de5c7328371a666bd437d81fcffb
XavierChanth <xchanthavong@gmail.com>
2021-06-18 11:24:00 -0400

    use sh_c fallback instead of sudo

--- create.sh
+++ create.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ $EUID != 0 ]; then
-    echo 'Error: please run this script as root.'
-fi
-
 if [[ $# -eq 0 || $# -gt 5 ]] ; then
     echo 'Usage create.sh <@sign> <fqdn> <port> <email> <service>'
     echo "Example create.sh @bob bob.example.com 6464 bob@example.com bob"
@@ -75,46 +71,62 @@ then
     exit 1
 fi
 
-# Confirm arguments look valid
-    tput setaf 2
-    echo "Creating $ATSIGN with $FQDN:$PORT with email $EMAIL with docker service name of $SERVICE"
-    tput setaf 9
+sh_c=''
+change_sh () {
+    if command_exists sudo; then
+        sh_c="sudo -E -u $1 sh -c"
+    elif command_exists su; then
+        sh_c="su -l $1 --preserve-environment -c"
+    else
+        echo 'Error: unable to perform operations as atsign user';
+        exit 1
+    fi
+}
 
+
+# Confirm arguments look valid
+tput setaf 2
+echo "Creating $ATSIGN with $FQDN:$PORT with email $EMAIL with docker service name of $SERVICE"
+tput setaf 9
+
+change_sh 'atsign'
 
 # Copy files in from base
-runuser -l atsign -c "mkdir -p /home/atsign/dess/$ATSIGN"
-runuser -l atsign -c "cp /home/atsign/base/.env /home/atsign/dess/$ATSIGN"
-runuser -l atsign -c "cp /home/atsign/base/docker-swarm.yaml /home/atsign/dess/$ATSIGN"
+$sh_c "mkdir -p /home/atsign/dess/$ATSIGN"
+$sh_c "cp /home/atsign/base/.env /home/atsign/dess/$ATSIGN"
+$sh_c "cp /home/atsign/base/docker-swarm.yaml /home/atsign/dess/$ATSIGN"
 # Make the directories in atsign
-runuser -l atsign -c "mkdir -p /home/atsign/atsign/$ATSIGN/storage"
+$sh_c "mkdir -p /home/atsign/atsign/$ATSIGN/storage"
 # Make the edits to the .env file
 # First comment out everything
-runuser -l atsign -c "sed -i 's/^\([^#].*\)/# \1/g' /home/atsign/dess/$ATSIGN/.env"
+$sh_c "sed -i 's/^\([^#].*\)/# \1/g' /home/atsign/dess/$ATSIGN/.env"
 # Add the environment variables we need
-echo "ATSIGN=$ATSIGN" |runuser -l atsign -c "tee -a  /home/atsign/dess/$ATSIGN/.env"
-echo "DOMAIN=$FQDN" |runuser -l atsign -c "tee -a /home/atsign/dess/$ATSIGN/.env"
-echo "PORT=$PORT" |runuser -l atsign -c "tee -a  /home/atsign/dess/$ATSIGN/.env"
-echo "EMAIL=$EMAIL" |runuser -l atsign -c "tee -a  /home/atsign/dess/$ATSIGN/.env"
-echo "SECRET=$SECRET" |runuser -l atsign -c "tee -a  /home/atsign/dess/$ATSIGN/.env"
+$sh_c "tee -a  /home/atsign/dess/$ATSIGN/.env <<< ATSIGN=$ATSIGN"
+$sh_c "tee -a /home/atsign/dess/$ATSIGN/.env <<< DOMAIN=$FQDN"
+$sh_c "tee -a  /home/atsign/dess/$ATSIGN/.env <<< PORT=$PORT"
+$sh_c "tee -a  /home/atsign/dess/$ATSIGN/.env <<< EMAIL=$EMAIL"
+$sh_c "tee -a  /home/atsign/dess/$ATSIGN/.env <<< SECRET=$SECRET"
 # copy over the .env file to base so we can renew the certs with an up to date EMAIL
-runuser -l atsign -c cp  ~atsign/dess/"$ATSIGN"/.env ~atsign/base/
+$sh_c "cp  /home/atsign/dess/$ATSIGN/.env /home/atsign/base/"
 # Get the certificate for the @sign
-    tput setaf 2
-    echo "Getting certificates"
-    tput setaf 9
+tput setaf 2
+echo "Getting certificates"
+tput setaf 9
 
-#     runuser -l atsign -c docker-compose --env-file ~atsign/dess/$ATSIGN/.env -f ~atsign/dess/$ATSIGN/docker-compose.yaml run  --service-ports cert
-sudo certbot certonly --standalone --domains "$FQDN" --non-interactive --agree-tos -m "$EMAIL"
+change_sh 'root'
+
+#     $sh_c docker-compose --env-file ~atsign/dess/$ATSIGN/.env -f ~atsign/dess/$ATSIGN/docker-compose.yaml run  --service-ports cert
+$sh_c "certbot certonly --standalone --domains $FQDN --non-interactive --agree-tos -m $EMAIL"
 
 # Last task to put in place the restart script and regenerate the ssl root CA file (as root)
 # Root CA
-sudo curl -L -o  /home/atsign/atsign/etc/live/"$FQDN"/cacert.pem https://curl.se/ca/cacert.pem
+$sh_c "curl -L -o  /home/atsign/atsign/etc/live/$FQDN/cacert.pem https://curl.se/ca/cacert.pem"
 # Put some ownership in place so atsign can read the certs
-sudo chown -R atsign:atsign "/home/atsign/atsign/$ATSIGN"
-sudo chown -R atsign:atsign "/home/atsign/atsign/etc/live/$FQDN"
-sudo chown -R atsign:atsign "/home/atsign/atsign/etc/archive/$FQDN"
+$sh_c "chown -R atsign:atsign /home/atsign/atsign/$ATSIGN"
+$sh_c "chown -R atsign:atsign /home/atsign/atsign/etc/live/$FQDN"
+$sh_c "chown -R atsign:atsign /home/atsign/atsign/etc/archive/$FQDN"
 # Copy over restart script
-sudo cp base/restart.sh ~atsign/atsign/etc/renewal-hooks/deploy
+$sh_c "cp base/restart.sh /home/atsign/atsign/etc/renewal-hooks/deploy"
 #
 #
 # We are now ready to start the secondary !
@@ -122,9 +134,10 @@ sudo cp base/restart.sh ~atsign/atsign/etc/renewal-hooks/deploy
 # It would be nice to use the @sign for the name but
 # Docker insists on a name that is DNS compliant and so emojis and @ signs are out hence the $SERVICE tag
 # we use a neat trick using docker-compose to create the compose file for us.
+change_sh 'atsign'
     echo Starting secondary for "$ATSIGN" at "$FQDN" on port "$PORT" as "$DNAME" on Docker
-runuser -l atsign -c "docker-compose --env-file /home/atsign/dess/$ATSIGN/.env -f /home/atsign/dess/$ATSIGN/docker-swarm.yaml config | tee ~atsign/dess/$ATSIGN/docker-compose.yaml > /dev/null"
-runuser -l atsign -c "docker stack deploy -c /home/atsign/dess/$ATSIGN/docker-compose.yaml $SERVICE"
+$sh_c "docker-compose --env-file /home/atsign/dess/$ATSIGN/.env -f /home/atsign/dess/$ATSIGN/docker-swarm.yaml config | tee ~atsign/dess/$ATSIGN/docker-compose.yaml > /dev/null"
+$sh_c "docker stack deploy -c /home/atsign/dess/$ATSIGN/docker-compose.yaml $SERVICE"
     echo Your QR-Code for "$ATSIGN"
     tput setaf 9
 qrencode -t ANSIUTF8 "${ATSIGN}:${SECRET}"
