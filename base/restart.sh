5e117c42600bc142bb4947d279acfd783ef302eb
Chris Swan <478926+cpswan@users.noreply.github.com>
2026-01-07 14:00:58 +0000

    feat: Add mTLS cert update using step CLI

--- base/restart.sh
+++ base/restart.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
-echo updating "$RENEWED_DOMAINS" certificates
+echo "Updating ${RENEWED_DOMAINS} certificates"
+LIVEDIR="~atsign/atsign/etc/live/${RENEWED_DOMAINS}"
+MTLSFC="${LIVEDIR}/mtls/fullchain.pem"
+MTLSPK="${LIVEDIR}/mtls/privkey.pem"
+ARCHIVEDIR="~atsign/atsign/etc/archive/${RENEWED_DOMAINS}"
+ATCASBASE="https://raw.githubusercontent.com/atsign-foundation/at_server/trunk"
+ATCASFILE="/packages/at_secondary_server/cacert/cacert.pem"
+# Get new mTLS certs
+mkdir -p "${LIVEDIR}/mtls"
+step ca certificate "${RENEWED_DOMAINS}" "${MTLSFC}" "${MTLSPK}"
 # Get the latest root certs (if they have changed)
-curl --time-cond  -L -o  ~atsign/atsign/etc//live/"$RENEWED_DOMAINS"/cacert.pem https://curl.se/ca/cacert.pem
-#
-# Touch the restart file to notify the Secondary that it has a new SSL certificate to install
-# It will do this when it has time and is not busy with other work
-# Get the ownership correct for atsign
+curl --time-cond  -L -o  "${LIVEDIR}"/cacert.pem "${ATCASBASE}${ATCASFILE}"
+# Touch the restart file to notify the atServer that it has a new TLS
+# certificate to install. It will do this when it has time and is not
+# busy with other work.
 touch ~atsign/atsign/etc/live/"$RENEWED_DOMAINS"/restart
-chown -R atsign:atsign ~atsign/atsign/etc/live/"$RENEWED_DOMAINS"
-chown -R atsign:atsign ~atsign/atsign/etc/archive/"$RENEWED_DOMAINS"
-
+# Get the ownership correct for atsign
+chown -R atsign:atsign "${LIVEDIR}"
+chown -R atsign:atsign "${ARCHIVEDIR}"
